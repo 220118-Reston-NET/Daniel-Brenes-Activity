39a35b39bb4f62cf0cede49f4c6ca6e04f49bcd3
dbrenes90 <daniel.brenes@my.tccd.edu>
2022-01-19 16:03:25 -0600

    finish day 2

---
+++ HelloWorld/Collection.cs
@@ -0,0 +1,85 @@
+namespace CollectionFunction
+{
+    public class Collection
+    {
+        //Array 
+        //Used to store a datatype and have a fixed size
+        //Syntax: (datatype)[] (name of variable) = new (datatype)[(size of the array)];
+        private int[] _nums = new int[5];
+        //Generic Collection
+        //They store a specific datatype and have variable size
+        //Syntax: <(datatype)> 
+
+        //List Collection
+        //zero-based index
+        //Used to store a datatype and has variable size
+        private List<string> _strings = new List<string>();
+
+        //Hashset Collection
+        //There is no particular order to the elementsso NOT zero-based index
+        //You cannot have duplicate elements 
+        private HashSet<int> numCollections = new HashSet<int>();
+
+        //Dictionary Collection
+        //Stores information through key-value pairs
+        //There is no particular order 
+        private Dictionary<string, int> directory = new Dictionary<string, int>();
+
+        //Non-generic Collection
+        //They store any datatype and have variable size
+        public void CollectionMain()
+        {
+            Console.WriteLine("==== Collection Demo====");
+            _nums[0] = 3;
+            _nums[1] = 10;
+            _nums[2] = 4;
+
+            foreach (var item in _nums)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            for (int i =0; i < _nums.Length; i++)
+            {
+                Console.WriteLine("Current value of i: " + i);
+                Console.WriteLine(_nums[i]);
+            }
+
+            Console.WriteLine("===Generic Collection===");
+            Console.WriteLine("==List Demo==");
+            _strings.Add("First element");
+            _strings.Add("Second element");
+            _strings.Add("Third element");
+
+            foreach (var item in _strings)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("=HashSet Demo");
+            numCollections.Add(1);
+            numCollections.Add(2);
+            numCollections.Add(3);
+            numCollections.Add(1);
+            
+            foreach (var item in numCollections)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("=Dictionary Demo=");
+            directory.Add("Daniel", -100);
+            directory.Add("Stephen", 100);
+            directory.Add("Vijhan", -10000);
+            directory.Add("Jonathon", 10000);
+
+            foreach(var item in directory)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,7 +1,43 @@
 ﻿using CarFunction;
+using CollectionFunction;
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-// NameOfClass NameOfVariable = new NameOfClass();
-Car car1 = new Car();
-Console.WriteLine(car1.Color);
+// // NameOfClass NameOfVariable = new NameOfClass();
+// Car car1 = new Car(100);
+// Console.WriteLine(car1.Color);
+// car1.Color = "Red";
+// Console.WriteLine(car1.Color);
+// car1.Fuel = 10;
+// Console.WriteLine(car1.Fuel);
+// car1.Start();
+// car1.Start(100);
+
+// Car car2 = new Car()
+// {   
+//     Color = "Yellow",
+//     Fuel = 1000,
+//     Owner = "Stephen"
+
+// };
+
+// Console.WriteLine("Welcome to programming!");
+// Console.WriteLine("Please tell me your name");
+// string name = Console.ReadLine();
+// Console.WriteLine($"Hello {name}, what do you want to do today?");
+// Console.WriteLine("[1] - Add two numbers");
+// Console.WriteLine("[2] - Exit");
+// string userInput = Console.ReadLine();
+// if (userInput == "1")
+// {
+//     Console.WriteLine("Please give me two numbers");
+//     int num1 = Convert.ToInt32(Console.ReadLine());
+//     int num2 = Convert.ToInt32(Console.ReadLine());
+
+//     Console.WriteLine($"The sum is: {num2+num1}");
+// }
+// else if (userInput == 2)
+
+// Initialized Collection class
+Collection collect = new Collection();
+collect.CollectionMain();
