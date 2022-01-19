46c40f77dadf596fa79f946bac5578fc3810e1b2
dbrenes90 <daniel.brenes@my.tccd.edu>
2022-01-19 13:02:54 -0600

    first init

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CarFunction;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// NameOfClass NameOfVariable = new NameOfClass();
+Car car1 = new Car();
+Console.WriteLine(car1.Color);
